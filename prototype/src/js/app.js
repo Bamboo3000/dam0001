1df1d6f54818c28f4bb936e2ab8c0366b4ed3a67
Jakub Pabis <pabis91@gmail.com>
2018-09-02 21:24:57 +0200

    finish homepage

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -47,3 +47,12 @@ function slideTo(el)
 		scrollTop: $(el).offset().top
 	}, 500);
 }
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1
+    });
+});
